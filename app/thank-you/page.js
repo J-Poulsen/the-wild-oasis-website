41264d777961852f9be26538902c6b18a856a3d4
Jens Poulsen <jpoulsen@iworq.com>
2025-01-10 16:25:32 -0700

    initial commit - create initial website layout

---
+++ app/thank-you/page.js
@@ -0,0 +1,19 @@
+export const metadata = {
+  title: 'Thank You',
+};
+
+export default function Page() {
+  return (
+    <div className='text-center space-y-6 mt-4'>
+      <h1 className='text-3xl font-semibold'>
+        Thank you for your reservation!
+      </h1>
+      <a
+        href='/account/reservations'
+        className='underline text-xl text-accent-500 inline-block'
+      >
+        Manage your reservations &rarr;
+      </a>
+    </div>
+  );
+}
